35f60122920d034770ae628ca43d314c6388251f
Kruti Boghara <mobileteam.sassy@gmail.com>
2023-02-21 14:35:02 +0530

    And View will appear.

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        //
+    }
 
 }
 
