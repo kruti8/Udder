98d4d5e39cd924924561f8b9cf8e00b3cdf54a21
Kruti Boghara <mobileteam.sassy@gmail.com>
2023-02-21 11:53:43 +0530

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Kruti on 21/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
